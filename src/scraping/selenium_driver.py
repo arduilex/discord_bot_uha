3aa3a1dbaa942fd3fb101cf5289849c56df3b454
Arduilex <alexandrebader3@gmail.com>
2024-01-21 19:42:12 +0100

    add help for firefox user

--- src/scraping/selenium_driver.py
+++ src/scraping/selenium_driver.py
@@ -2,12 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 # see https://pypi.org/project/webdriver-manager/
+
+## If you use chromium
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
+## If you use firefox
+# from selenium.webdriver.firefox.service import Service as FirefoxService
+# from webdriver_manager.firefox import GeckoDriverManager
+
 def driver_on():
     options = Options()
     options.add_argument("--headless")
-    # options.add_argument("--disable-gpu")
+    ## If you use chromium
     return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+
+    ## If you use firefox
+    # return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
---
+++ src/test/driver_firefox.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+# see https://pypi.org/project/webdriver-manager/
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+options = Options()
+options.add_argument("--headless")  # Assurez-vous d'utiliser deux tirets
+# options.add_argument("--disable-gpu")
+driver = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://google.com")
+print("Titre de la page: {}".format(driver.title))
+# driver.save_screenshot('screenshot.png')
+
+input("entrez pour quitter")
+driver.quit()

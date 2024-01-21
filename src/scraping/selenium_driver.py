2b65a94593eb61a6935a1858d15ff1376d6fdaf3
Arduilex <alexandrebader3@gmail.com>
2024-01-21 22:23:05 +0100

    major fix for firefox driver

--- src/scraping/selenium_driver.py
+++ src/scraping/selenium_driver.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 # see https://pypi.org/project/webdriver-manager/
 
 ## If you use chromium
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 ## If you use firefox
+# from selenium.webdriver.firefox.options import Options
 # from selenium.webdriver.firefox.service import Service as FirefoxService
 # from webdriver_manager.firefox import GeckoDriverManager
 
@@ -19,4 +20,4 @@ def driver_on():
     return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
     ## If you use firefox
-    # return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    # return webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))

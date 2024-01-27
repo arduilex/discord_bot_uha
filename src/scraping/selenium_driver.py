f5f3825bd59a1f8d2d244240559114a325279866
arduilex <alexandrebader3@gmail.com>
2024-01-27 16:55:58 +0100

    defaut Firefox

--- src/scraping/selenium_driver.py
+++ src/scraping/selenium_driver.py
@@ -3,21 +3,21 @@ from selenium import webdriver
 # see https://pypi.org/project/webdriver-manager/
 
 ## If you use chromium
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromiumService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.service import Service as ChromiumService
+#from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.core.os_manager import ChromeType
 
 ## If you use firefox
-# from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.firefox.service import Service as FirefoxService
-# from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 def driver_on():
     options = Options()
     options.add_argument("--headless")
     ## If you use chromium
-    return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    # return webdriver.Chrome(options=options, service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
     ## If you use firefox
-    # return webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))
+    return webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))

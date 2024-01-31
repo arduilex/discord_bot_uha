182fbf0c9be60c3f5bdaafeed25da2c887388638
arduilex <alexandrebader3@gmail.com>
2024-01-31 09:56:43 +0100

    add sleep + edit logging

--- src/scraping/crous.py
+++ src/scraping/crous.py
@@ -26,16 +26,20 @@ def get_crous():
         driver = driver_on()
         skip_cookie(driver)
         driver.get("https://www.crous-strasbourg.fr/restaurant/resto-u-de-liut-mulhouse-2/")
+        logging.info("crous access")
         today_date = datetime.now().strftime("%-d")
         next_day_button = driver.find_element(By.CLASS_NAME, "next")
         menu_dict = get_day_menu(driver)
         if not today_date in menu_dict["date"]:
             next_day_button.click()
-            time.sleep(1)
+            time.sleep(2)
             menu_dict = get_day_menu(driver)
         with open("data/raw_menu.txt", "w", encoding='utf-8') as file:
-                file.write("\n".join(menu_dict["menu"]))
-                logging.info(f"{menu_dict['date']} créé !")
+            file.write("\n".join(menu_dict["menu"]))
+        logging.info(f"{menu_dict['date']} trouvé !")
         driver.quit()
     except:
         logging.error("Impossible d'accéder au site crous")
+        driver.quit()
+        time.sleep(5)
+        get_crous()

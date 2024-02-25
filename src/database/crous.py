cfb80c02fdc6a107a7274dadca6061b2004e2c8e
arduilex <alexandrebader3@gmail.com>
2024-02-25 12:02:02 +0100

    add try

--- src/database/crous.py
+++ src/database/crous.py
@@ -11,26 +11,26 @@ def create_menu():
         "Pasta Box": [],
         "Origines de nos viandes du jour": []
     }
-    with open("data/raw_menu.txt", encoding='utf-8') as file:
-        current_menu = None
-        try:
+    try:
+        with open("data/raw_menu.txt", encoding='utf-8') as file:
+            current_menu = None
             for line in file:
                 line = line.strip()
                 if line in new_menus:
                     current_menu = line
                 elif current_menu is not None:
                     new_menus[current_menu].append(line)
-        except:
-            logging.error("erreur lors de l'extraction du menu raw")
-    # supression des clés vides
-    for key in list(new_menus.keys()):
-        if not new_menus[key]:
-            del new_menus[key]
-    if new_menus["Origines de nos viandes du jour"]:
-        del new_menus["Origines de nos viandes du jour"]
-    with open('data/menus.json', "w", encoding='utf-8') as file:
-        json.dump(new_menus, file, ensure_ascii=False, indent=4)
-    logging.info("menu du crous json créé !")
+        # supression des clés vides
+        for key in list(new_menus.keys()):
+            if not new_menus[key]:
+                del new_menus[key]
+        if new_menus["Origines de nos viandes du jour"]:
+            del new_menus["Origines de nos viandes du jour"]
+        with open('data/menus.json', "w", encoding='utf-8') as file:
+            json.dump(new_menus, file, ensure_ascii=False, indent=4)
+        logging.info("menu du crous json créé !")
+    except:
+        logging.error("erreur lors de l'extraction du menu raw")
 
 def is_closed():
     with open("data/raw_menu.txt") as file:
--- src/scraping/crous.py
+++ src/scraping/crous.py
@@ -11,36 +11,38 @@ class CrousScrap():
     def get_day_menu(self, driver):
         try:
             menu_obj = driver.find_elements(By.XPATH, "//*[@class='menu slick-slide slick-current slick-active']")
+            # menu_obj = driver.find_elements(By.CLASS_NAME, "meal")
             menu_str = menu_obj[0].text
             self.crous_date, *self.crous_menu = menu_str.split('\n')
+            print(self.crous_menu)
         except:
             logging.error("Impossible de récupérer le menu")
 
     def get_crous(self):
         driver = driver_on()
-        # try:
-        driver.get("https://www.crous-strasbourg.fr/restaurant/resto-u-de-liut-mulhouse/")
-        logging.info("crous website loaded sucessfull")
-        # Add cookie to skip popup
-        cookies = pickle.load(open("data/cookies.pkl", "rb"))
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-        driver.refresh()
-        today_date_number = datetime.now().strftime("%-d")
-        today_date_number = "6"
-        self.get_day_menu(driver)
-        while not today_date_number in self.crous_date:
-            next_day_button = driver.find_element(By.CLASS_NAME, "next")
-            next_day_button.click()
-            logging.info("clic sur le bouton suivant")
-            time.sleep(1)
+        try:
+            driver.get("https://www.crous-strasbourg.fr/restaurant/resto-u-de-liut-mulhouse/")
+            logging.info("crous website loaded sucessfull")
+            # Add cookie to skip popup
+            cookies = pickle.load(open("data/cookies.pkl", "rb"))
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.refresh()
+            today_date_number = datetime.now().strftime("%-d")
+            # today_date_number = "9"
             self.get_day_menu(driver)
-        with open("data/raw_menu.txt", "w", encoding='utf-8') as file:
-            file.write("\n".join(self.crous_menu))
-        logging.info(f"{self.crous_date} trouvé !")
-        driver.quit()
-        # except:
-        #     logging.error("erreur lors de l'accès au site crous")
-        #     driver.quit()
-        #     time.sleep(5)
-        #     self.get_crous()
+            if not today_date_number in self.crous_date:
+                next_day_button = driver.find_element(By.CLASS_NAME, "next")
+                next_day_button.click()
+                logging.info("clic sur le bouton suivant")
+                time.sleep(1)
+                self.get_day_menu(driver)
+            with open("data/raw_menu.txt", "w", encoding='utf-8') as file:
+                file.write("\n".join(self.crous_menu))
+            logging.info(f"{self.crous_date} trouvé !")
+            driver.quit()
+        except:
+            logging.error("erreur lors de l'accès au site crous")
+            driver.quit()
+            time.sleep(5)
+            self.get_crous()
